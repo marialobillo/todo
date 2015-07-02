c4a5230743a857346f89ff2c649813210b609658
Neniah <maria.lobillo.santos@gmail.com>
2015-07-02 20:53:53 +0200

    todo items

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,20 @@
+class TodoItemsController < ApplicationController
+  before_action
+
+  def create
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
+  end
+
+
+  private
+
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
+    def todo_item_params
+      params[:todo_item].permit(:content)
+    end
+
+end
